cc0595fd45e67922b22f8c6b16f32a01b65ea647
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-04-19 12:11:41 -0700

    Add Otel Go SDK to go fn

--- go/go.mod
+++ go/go.mod
@@ -2,4 +2,12 @@ module tammy-test-go
 
 go 1.21.4
 
-require github.com/aws/aws-lambda-go v1.46.0 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.47.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda v0.50.0 // indirect
+	go.opentelemetry.io/otel v1.25.0 // indirect
+	go.opentelemetry.io/otel/metric v1.25.0 // indirect
+	go.opentelemetry.io/otel/trace v1.25.0 // indirect
+)
--- go/main.go
+++ go/main.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
 )
 
 // MyEvent represents input to the Lambda function.
@@ -17,14 +18,18 @@ type MyEvent struct {
 // and maybe an error.
 func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
 	if event == nil {
-		return nil, fmt.Errorf("received a nil event")
+		message := "received a nil event"
+		log.Println(message)
+		return nil, fmt.Errorf(message)
 	}
 	message := fmt.Sprintf("Hello %s!", event.Name)
+	log.Println(message)
 	return &message, nil
 }
 
-// main is the entry point to our Lambda function.
+// main is the entry point to our Lambda function,
+// wrapped by OpenTelemetry Go SDK.
 func main() {
 	log.Println("Starting HandleRequest")
-	lambda.Start(HandleRequest)
+	lambda.Start(otellambda.InstrumentHandler(HandleRequest))
 }
