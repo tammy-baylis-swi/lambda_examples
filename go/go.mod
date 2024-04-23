8ef82de62ab43c017a0f3e8571430a169186a6b1
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2024-04-23 16:02:12 -0700

    Add go foo metrics

--- go/go.mod
+++ go/go.mod
@@ -5,8 +5,11 @@ go 1.21.4
 require (
 	github.com/aws/aws-lambda-go v1.47.0
 	go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda v0.50.0
+	go.opentelemetry.io/otel v1.25.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.25.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.25.0
 	go.opentelemetry.io/otel/sdk v1.25.0
+	go.opentelemetry.io/otel/sdk/metric v1.25.0
 	go.opentelemetry.io/otel/trace v1.25.0
 )
 
@@ -15,7 +18,6 @@ require (
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
-	go.opentelemetry.io/otel v1.25.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.25.0 // indirect
 	go.opentelemetry.io/otel/metric v1.25.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
--- go/main.go
+++ go/main.go
@@ -4,10 +4,14 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/aws/aws-lambda-go/lambda"
 	"go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	"go.opentelemetry.io/otel/trace"
 )
@@ -22,6 +26,30 @@ type MyEvent struct {
 // HandleRequest returns a pointer to a string containing invoke result
 // and maybe an error.
 func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+	// otellambda does not have `WithMeterProvider` like with `WithTracerProvider`
+	// https://pkg.go.dev/go.opentelemetry.io/contrib/instrumentation/github.com/aws/aws-lambda-go/otellambda@v0.50.0#section-readme
+	httpMetricExporter, _ := otlpmetrichttp.New(ctx)
+	metricReader := sdkmetric.NewPeriodicReader(httpMetricExporter,
+		// If internal is less than or equal to zero, 60s default is used
+		// https://pkg.go.dev/go.opentelemetry.io/otel/sdk/metric#WithInterval
+		sdkmetric.WithInterval(1*time.Second))
+	meterProvider := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(metricReader))
+	// Register as global meter provider so that it can be used via otel.Meter
+	// and accessed using otel.GetMeterProvider.
+	// Most instrumentation libraries use the global meter provider as default.
+	// If the global meter provider is not set then a no-op implementation
+	// is used, which fails to generate data.
+	otel.SetMeterProvider(meterProvider)
+
+	var meter = otel.Meter("foo-meter")
+	lambdaStartCounter, _ := meter.Int64Counter("foo.tammy.test.count")
+	lambdaStartCounter.Add(
+		ctx,
+		3)
+	// Required with PeriodicReader interval 1s
+	metricReader.ForceFlush(ctx)
+
 	// Manually create child span
 	_, span := tracer.Start(ctx, "my-manual-span")
 	defer span.End()
@@ -41,10 +69,12 @@ func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
 func main() {
 	log.Println("Starting HandleRequest")
 
-	httpTraceExporter, _ := otlptracehttp.New(context.Background())
+	ctx := context.Background()
+	httpTraceExporter, _ := otlptracehttp.New(ctx)
 	traceProvider := sdktrace.NewTracerProvider(
 		sdktrace.WithSyncer(httpTraceExporter))
 
+	// set for manual trace creation
 	tracer = traceProvider.Tracer("MyLambdaService")
 
 	lambda.Start(
